10153b9537d57649f6ed4eb2bcda144351db7919
wizardry <rein1988@gmail.com>
2023-08-24 03:58:43 +0900

    hide hello world

---
+++ src/layouts/default-layout.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="main">
+    <header class="header">Wiz'z vite training app hoge</header>
+    <div class="content">
+      <slot />
+    </div>
+    <footer class="footer">twitter by @wiz_rein</footer>
+  </div>
+</template>
+<style scoped>
+.main {
+  --header-height: 40px;
+  --footer-height: 40px;
+  .header {
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    padding: 0 18px;
+    height: var(--header-height);
+    background: var(--upBg);
+    border-bottom: 1px solid #ccc;
+  }
+
+  .content {
+    box-sizing: border-box;
+    padding: 24px 18px;
+    min-height: calc(100vh - var(--header-height) - var(--footer-height));
+  }
+
+  .footer {
+    box-sizing: border-box;
+    display: flex;
+    justify-content: end;
+    align-items: center;
+    padding: 0 18px;
+    height: 40px;
+    background: var(--upBg);
+    border-top: 1px solid #ccc;
+    font-size: 10px;    
+  }
+}
+</style>

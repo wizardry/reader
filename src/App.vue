5096811ad1d1e4e89daf28f60684b3427cd6f230
wizardry <rein1988@gmail.com>
2023-08-25 05:43:15 +0900

    雑にRSS取得したXMLを表示するまで

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,52 @@
 <script setup lang="ts">
+import { ref, reactive } from 'vue';
 import DefaultLayout from '@/layouts/default-layout.vue';
 // 文字列として展開してjsの世界で展開する
 import rssData from '@/assets/dist/rss.xml?raw';
-console.log(rssData);
+import { XMLParser } from 'fast-xml-parser';
+
+const parser = new XMLParser();
+const parsedData = parser.parse(rssData);
+console.log(parsedData["rdf:RDF"]);
+const feedTitle = ref(parsedData["rdf:RDF"].channel.title);
+const feed = reactive(parsedData["rdf:RDF"].item);
+
 </script>
 
 <template>
   <DefaultLayout>
-    <article class="basic-list">
-      <header class="basic-list_header">
-        <h1 class="basic-list_title">あいうえお</h1>
-      </header>
-      <section class="basic-item">
-        <a class="basic-item_anchor" href="">
-          <h1 class="basic-item_heading">heading</h1>
-        </a>
-        <time
-          class="basic-item_date"
-          :datetime="'2023-01-01'"
-        >2023-01-01</time>
-        <p class="basic-item_description">descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription</p>
-      </section>
-      <section class="basic-item">
-        <a class="basic-item_anchor" href="">
-          <h1 class="basic-item_heading">heading</h1>
-        </a>
-        <time
-          class="basic-item_date"
-          :datetime="'2023-01-01'"
-        >2023-01-01</time>
-        <p class="basic-item_description">descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription</p>
-      </section>
-    </article>
+    <div class="articles-wrap">
+      <article class="basic-list">
+        <header class="basic-list_header">
+          <h1 class="basic-list_title">{{ feedTitle }}</h1>
+        </header>
+        <section v-for="(item, index) in feed" :key="index" class="basic-item">
+          <time
+            class="basic-item_date"
+            :datetime="item['dc:date']"
+          >{{ item["dc:date"] }}</time>
+          <a class="basic-item_anchor" href="item.link">
+            <h1 class="basic-item_heading">{{ item.title }}</h1>
+          </a>
+          <p class="basic-item_description">{{ item.description }}</p>
+        </section>
+      </article>
+    </div>
   </DefaultLayout>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 
+.articles-wrap {
+  display: flex;
+  align-items: stretch;
+}
 .basic-list {
-
+  width: 300px;
+  padding: 16px;
+  border: 1px solid var(--baseBorder);
+  border-radius: 3px;
+  background: var(--downBg);
   .basic-list_header {
     display: flex;
 
@@ -56,11 +64,16 @@ console.log(rssData);
   padding-top: 12px;
   border-top: 1px solid var(--baseBorder);
 
+  .basic-item_anchor {
+    color: inherit;
+    text-decoration: underline;
+  }
   .basic-item_heading {
     margin: 0;
     padding-top: 8px;
     letter-spacing: 0.15em;
     font-size: 18px;
+    line-height: 1.6;
   }
 
   .basic-item_date {
@@ -72,6 +85,7 @@ console.log(rssData);
     line-height: 1.6;
     font-size: 14px;
     letter-spacing: 0.15em;
+    word-break: break-all;
   }
 }
 

b782907844c09a86440aaa2c2d29f2faafe17af9
wizardry <rein1988@gmail.com>
2023-08-25 02:47:48 +0900

    fix tslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import postcssNesting from "postcss-nesting";
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@/': `${__dirname}/src/`,
+      '@': path.resolve(__dirname, 'src/')
     },
   },
   css: {

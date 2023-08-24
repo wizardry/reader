261bba380ff520f11f30300ab0bef67e883df9ef
wizardry <rein1988@gmail.com>
2023-08-25 02:50:06 +0900

    set easy style

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,80 @@
 <script setup lang="ts">
-import DefaultLayout from './layouts/default-layout.vue';
+import DefaultLayout from '@/layouts/default-layout.vue';
+// 文字列として展開してjsの世界で展開する
+import rssData from '@/assets/dist/rss.xml?raw';
+console.log(rssData);
 </script>
 
 <template>
   <DefaultLayout>
-    hogehoge
+    <article class="basic-list">
+      <header class="basic-list_header">
+        <h1 class="basic-list_title">あいうえお</h1>
+      </header>
+      <section class="basic-item">
+        <a class="basic-item_anchor" href="">
+          <h1 class="basic-item_heading">heading</h1>
+        </a>
+        <time
+          class="basic-item_date"
+          :datetime="'2023-01-01'"
+        >2023-01-01</time>
+        <p class="basic-item_description">descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription</p>
+      </section>
+      <section class="basic-item">
+        <a class="basic-item_anchor" href="">
+          <h1 class="basic-item_heading">heading</h1>
+        </a>
+        <time
+          class="basic-item_date"
+          :datetime="'2023-01-01'"
+        >2023-01-01</time>
+        <p class="basic-item_description">descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription</p>
+      </section>
+    </article>
   </DefaultLayout>
 </template>
 
 <style scoped>
+
+.basic-list {
+
+  .basic-list_header {
+    display: flex;
+
+    &:nth-child(n + 2) {
+      margin-top: 12px;
+    }
+  }
+  .basic-list_title {
+    margin: 0;
+    font-size: 20px;
+  }
+}
+.basic-item {
+  margin-top: 12px;
+  padding-top: 12px;
+  border-top: 1px solid var(--baseBorder);
+
+  .basic-item_heading {
+    margin: 0;
+    padding-top: 8px;
+    letter-spacing: 0.15em;
+    font-size: 18px;
+  }
+
+  .basic-item_date {
+    display: block;
+    font-size: 12px;
+  }
+
+  .basic-item_description {
+    line-height: 1.6;
+    font-size: 14px;
+    letter-spacing: 0.15em;
+  }
+}
+
 .logo {
   height: 6em;
   padding: 1.5em;
